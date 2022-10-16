1d617e76af6f76f3f2ed8206642e09abbc142941
Thiago Avila <hey@thiagoavila.dev>
2022-10-16 09:14:35 -0300

    remove code action lightbulb sign

--- neovim/.config/nvim/plugin/lspsaga.lua
+++ neovim/.config/nvim/plugin/lspsaga.lua
@@ -1,25 +1,22 @@
 local status_saga, saga = pcall(require, 'lspsaga')
 if (not status_saga) then return end
 
-saga.init_lsp_saga {}
+saga.init_lsp_saga {
+  code_action_lightbulb = {
+    enable = true,
+    sign = false,
+    virtual_text = true,
+  },
+}
 
 -- Mappings
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-local diagnostic = require('lspsaga.diagnostic')
-map('n', '<C-j>', function() diagnostic.goto_next({}) end, opts)
-
-local hover = require('lspsaga.hover')
-map('n', 'K', function() hover.render_hover_doc() end, opts)
-
-local finder = require('lspsaga.finder')
-map('n', 'gd', function() finder:lsp_finder() end, opts)
-
+map('n', '<C-j>', function() require('lspsaga.diagnostic').goto_next({}) end, opts)
+map('n', 'K', function() require('lspsaga.hover'):render_hover_doc() end, opts)
+map('n', 'gd', function() require('lspsaga.finder'):lsp_finder() end, opts)
 map('i', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
-
-local definition = require('lspsaga.definition')
-map('n', 'gp', function() definition.preview_definition() end, opts)
-
-local rename = require('lspsaga.rename')
-map('n', 'gr', function() rename.lsp_rename() end, opts)
+map('n', 'gp', function() require('lspsaga.definition'):preview_definition() end, opts)
+map('n', 'gr', function() require('lspsaga.rename'):lsp_rename() end, opts)
+map('n', 'ca', function() require('lspsaga.codeaction'):code_action() end, opts)
