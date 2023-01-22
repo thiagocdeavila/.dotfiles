b8addd634bf7ec28d9c0e869876a50207db7ab99
Thiago Avila <hey@thiagoavila.dev>
2023-01-22 13:25:58 -0300

    Replace packer.nvim with lazy.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,58 +1,60 @@
 require('bootstrap')
 
-local packer = require('packer')
+local lazy = require('lazy')
 
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
+lazy.setup({
+  'nvim-lua/plenary.nvim',
 
   -- Telescope
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x' }
-  use 'nvim-telescope/telescope-file-browser.nvim'
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x' },
+  'nvim-telescope/telescope-file-browser.nvim',
 
   -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use { 'folke/trouble.nvim' }
+  'neovim/nvim-lspconfig',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'jose-elias-alvarez/null-ls.nvim',
+  'folke/trouble.nvim',
 
   -- Autocompletation
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+    }
+  },
 
   -- Snippets
-  use 'L3MON4D3/LuaSnip'
+  'L3MON4D3/LuaSnip',
 
   -- Debugging
-  use 'mfussenegger/nvim-dap'
+  'mfussenegger/nvim-dap',
 
   -- Color Themes
-  use 'rebelot/kanagawa.nvim'
-  use { 'catppuccin/nvim', as = 'catppuccin' }
+  'rebelot/kanagawa.nvim',
+  { 'catppuccin/nvim', name = 'catppuccin' },
 
   -- UI
-  use { 'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end }
-  use 'kyazdani42/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  'kyazdani42/nvim-web-devicons',
+  'nvim-lualine/lualine.nvim',
 
   -- Git integration
-  use 'lewis6991/gitsigns.nvim'
+  'lewis6991/gitsigns.nvim',
 
   -- Language Tools
-  use 'akinsho/flutter-tools.nvim'
-  use 'posva/vim-vue'
-  use 'pearofducks/ansible-vim'
-  use 'folke/neodev.nvim' -- LSP for nvim lua API
+  { 'akinsho/flutter-tools.nvim', ft = 'dart' },
+  { 'posva/vim-vue', ft = 'vue' },
+  { 'folke/neodev.nvim', ft = 'lua' },
 
-  use 'norcalli/nvim-colorizer.lua' -- Color Highlighter
-  use 'editorconfig/editorconfig-vim' -- Add .editorconfig support
-  use 'numToStr/Comment.nvim' -- Comment plugin
-  use 'folke/todo-comments.nvim' -- Highlight and search for todo comments
-  use 'windwp/nvim-autopairs' -- Autopair plugin
-  use 'andymass/vim-matchup' -- Navigate and highlight matching words
-  use 'tpope/vim-surround'
-end)
+  'norcalli/nvim-colorizer.lua', -- Color Highlighter
+  'editorconfig/editorconfig-vim', -- Add .editorconfig support
+  'numToStr/Comment.nvim', -- Comment plugin
+  'folke/todo-comments.nvim', -- Highlight and search for todo comments
+  'windwp/nvim-autopairs', -- Autopair plugin
+  'andymass/vim-matchup', -- Navigate and highlight matching words
+  'tpope/vim-surround',
+})
