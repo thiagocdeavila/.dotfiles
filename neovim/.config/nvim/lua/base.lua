0366c39ed7dd31bbf5a3332e15fbfb35e390f82f
Thiago Avila <hey@thiagoavila.dev>
2023-05-10 10:28:40 -0300

    feat(neovim): add base configuration and lazy.nvim setup

---
+++ neovim/.config/nvim/lua/base.lua
@@ -0,0 +1,28 @@
+vim.cmd('autocmd!')
+
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'zsh'
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = false
+vim.opt.si = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.g.netrw_banner = 0
