85bf082c9c644429fb965eca772fe5b6e478af0d
Thiago Avila <hey@thiagoavila.dev>
2023-05-10 11:15:12 -0300

    feat(neovim): add nvim-treesitter plugin

---
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = 'BufReadPost',
+    opts = {
+      ensure_installed = 'all',
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
+}
